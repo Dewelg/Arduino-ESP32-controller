c402a0de417aba75bf3fb1ad62fdec96882b7bc7
Dewel Gonzalez <dewel713@gmail.com>
2025-05-13 21:34:38 -0400

    spi pin set up set for master board

--- esp-spi/src/masterSPI.cpp
+++ esp-spi/src/masterSPI.cpp
@@ -7,11 +7,12 @@ SPIClass SPI2(VSPI);
 void setup() {
   Serial.begin(115200);
 
+  //enable spi and config pins
   SPI2.begin(18, 19, 23, 5);
   pinMode(5, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
 
